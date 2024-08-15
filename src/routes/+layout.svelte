9d9833a436a63bf220765e3aa4df859a09851f81
ejherzog <23402385+ejherzog@users.noreply.github.com>
2024-08-15 12:28:18 -0400

    sveltestrap, logo, favicon

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Navbar, Container, Image } from '@sveltestrap/sveltestrap';
+</script>
+
+<Navbar style="background-color: #0b473b;">
+    <Image class="mx-auto w-50" src="logo/logo-cropped.svg" alt="Logo for EvA: Every Attendee" />
+</Navbar>
+
+<Container class="text-center mt-1 mb-4">
+	<slot></slot>
+</Container>
+
+<Container class="text-center mt-1 mb-4">
+	<hr />
+	<p>
+		EveryAttendee is the brainchild of <a href="https://github.com/ejherzog" class="text-reset" target="_blank">ejherzog</a>.
+	</p>
+</Container>
