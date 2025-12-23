3bb019dd0218995bdc0c4e09bf6726d3ff0e2e44
ejherzog <23402385+ejherzog@users.noreply.github.com>
2025-12-23 10:22:44 -0500

    remove debug line

--- src/routes/api/logout/+server.ts
+++ src/routes/api/logout/+server.ts
@@ -5,6 +5,5 @@ export const POST: RequestHandler = async (event) => {
     
     invalidateSession(event.locals.session.sessionId);
     event.cookies.delete("session", { path: "/" });
-    console.log("successfully logged out");
     redirect(303, '/');
 }
